b52a82273346ec38094109eb9474c952c255e555
aliraza0908 <maliraza123rt@gmail.com>
2026-07-16 19:43:43 +0500

    Use placeholder values in config.py, real keys via env vars

---
+++ config.py
@@ -0,0 +1,5 @@
+﻿import os
+
+ASSEMBLY_API_KEY = os.environ.get("ASSEMBLY_API_KEY", "your_assembly_key_here")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "your_groq_key_here")
+LLM_MODEL = os.environ.get("LLM_MODEL", "llama-3.3-70b-versatile")
